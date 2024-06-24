2574a2d3a36229733a99ccc0a90a057102210fb9
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2024-06-24 22:18:42 +0200

    nova baza

--- ecommerce.sql
+++ ecommerce.sql
@@ -14,10 +14,22 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 -- Dumping database structure for ecommerce
 CREATE DATABASE IF NOT EXISTS `ecommerce` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `ecommerce`;
+
+-- Dumping structure for table ecommerce.tip
+CREATE TABLE IF NOT EXISTS `tip` (
+  `tip_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tip` varchar(255) NOT NULL,
+  PRIMARY KEY (`tip_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table ecommerce.tip: ~3 rows (approximately)
+INSERT INTO `tip` (`tip_id`, `tip`) VALUES
+	(1, 'Akusticna'),
+	(2, 'Elektricna'),
+	(3, 'Klasicna');
 
 -- Dumping structure for table ecommerce.gitara
 CREATE TABLE IF NOT EXISTS `gitara` (
@@ -34,36 +46,6 @@ CREATE TABLE IF NOT EXISTS `gitara` (
 INSERT INTO `gitara` (`gitara_id`, `tip_id`, `cena`, `ime`) VALUES
 	(2, 2, 222, 'Gitara1'),
 	(3, 1, 100, 'Gitara4');
-
--- Dumping structure for table ecommerce.korpa
-CREATE TABLE IF NOT EXISTS `korpa` (
-  `korpa_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `gitara_id` int(10) unsigned NOT NULL,
-  `kolicina` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`korpa_id`),
-  KEY `fk_korpa_user_id` (`user_id`),
-  KEY `fk_korpa_gitara_id` (`gitara_id`),
-  CONSTRAINT `fk_korpa_gitara_id` FOREIGN KEY (`gitara_id`) REFERENCES `gitara` (`gitara_id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_korpa_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Dumping data for table ecommerce.korpa: ~1 rows (approximately)
-INSERT INTO `korpa` (`korpa_id`, `user_id`, `gitara_id`, `kolicina`) VALUES
-	(2, 2, 3, 5);
-
--- Dumping structure for table ecommerce.tip
-CREATE TABLE IF NOT EXISTS `tip` (
-  `tip_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `tip` varchar(255) NOT NULL,
-  PRIMARY KEY (`tip_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Dumping data for table ecommerce.tip: ~3 rows (approximately)
-INSERT INTO `tip` (`tip_id`, `tip`) VALUES
-	(1, 'Akusticna'),
-	(2, 'Elektricna'),
-	(3, 'Klasicna');
 
 -- Dumping structure for table ecommerce.uloga
 CREATE TABLE IF NOT EXISTS `uloga` (
@@ -94,6 +76,23 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`user_id`, `uloga_id`, `ime_prezime`, `email`, `password_hash`, `is_active`) VALUES
 	(1, 1, 'luka', 'luka@gmail.com', 'luka123', 1),
 	(2, 2, 'ogi', 'ogi@gmail.com', 'ogi123', 1);
+
+-- Dumping structure for table ecommerce.korpa
+CREATE TABLE IF NOT EXISTS `korpa` (
+  `korpa_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `gitara_id` int(10) unsigned NOT NULL,
+  `kolicina` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`korpa_id`),
+  KEY `fk_korpa_user_id` (`user_id`),
+  KEY `fk_korpa_gitara_id` (`gitara_id`),
+  CONSTRAINT `fk_korpa_gitara_id` FOREIGN KEY (`gitara_id`) REFERENCES `gitara` (`gitara_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_korpa_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table ecommerce.korpa: ~1 rows (approximately)
+INSERT INTO `korpa` (`korpa_id`, `user_id`, `gitara_id`, `kolicina`) VALUES
+	(2, 2, 3, 5);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
